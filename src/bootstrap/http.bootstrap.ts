f6ce723f4f55862b8ccee008865bd04c1137d804
Erickson Manuel Holguin <ericksonmanuel.holguin@gmail.com>
2026-02-16 08:53:39 -0400

    Add Vercel configuration and refactor app bootstrap
    - Introduced a new vercel.json file to specify the schema and bun version.
    - Updated the app bootstrap process to export the app instance for better modularity.
    - Removed the direct listening of the HTTP server from the bootstrapHttp function to streamline server initialization.

--- src/bootstrap/http.bootstrap.ts
+++ src/bootstrap/http.bootstrap.ts
@@ -2,7 +2,5 @@ import type { Elysia } from "elysia";
 import { env } from "../config/env";
 
 export const bootstrapHttp = async (app: Elysia) => {
-  app.listen(env.PORT);
-
   console.log(`🚀 HTTP server running on http://localhost:${env.PORT}`);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import "reflect-metadata";
 import { bootstrapApp } from "./bootstrap/app.bootstrap";
 
-await bootstrapApp();
+const app = await bootstrapApp();
+
+export default app;
